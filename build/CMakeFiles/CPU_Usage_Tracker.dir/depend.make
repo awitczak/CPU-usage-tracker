7e7f7160b1bf38fcf65e21a29abc7ab077044aeb
Adam Witczak <74379133+awitczak@users.noreply.github.com>
2023-03-15 23:32:19 +0100

    Clean-up

--- build/CMakeFiles/CPU_Usage_Tracker.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CPU_Usage_Tracker.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/awitczak/Desktop/CPU_Usage_Tracker
-# Build directory: /home/awitczak/Desktop/CPU_Usage_Tracker/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
